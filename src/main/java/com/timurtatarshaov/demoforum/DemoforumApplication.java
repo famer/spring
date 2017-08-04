cc2501dab1a372d7d786ec44db6ee3d77b4a8e32
Timur Tatarshaov <tatarshaov@gmail.com>
2017-08-04 17:22:05 +0300

    init

---
+++ src/main/java/com/timurtatarshaov/demoforum/DemoforumApplication.java
@@ -0,0 +1,12 @@
+package com.timurtatarshaov.demoforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoforumApplication.class, args);
+	}
+}
